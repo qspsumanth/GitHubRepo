f4d3cfcaf659136502881ac169bfbf8744d0c659
qspsumanth <sumanth12897@gmail.com>
2023-06-12 20:17:52 +0530

    First Commit....

---
+++ src/test/java/Example/Example1.java
@@ -0,0 +1,18 @@
+package Example;
+
+import org.testng.annotations.Test;
+
+
+public class Example1 {
+	
+	@Test
+	public void add() {
+	int a=10;
+	int b=20;
+	int c=a+b;
+	System.out.println(c);
+	
+	}
+	
+
+}
---
+++ src/test/java/Example/Example2.java
@@ -0,0 +1,14 @@
+package Example;
+
+import org.testng.annotations.Test;
+
+public class Example2 {
+	@Test
+	public void mul() {
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println(c);
+	}
+
+}
